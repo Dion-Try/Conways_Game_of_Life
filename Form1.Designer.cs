c47e8bd7649d8f2e4f893ee0fa10626f14fd9b1b
Dion-Try <61267319+Dion-Try@users.noreply.github.com>
2021-09-29 10:04:39 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Conways_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReset = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(569, 178);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(95, 54);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(569, 85);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(95, 54);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.btnReset);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conways_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static int columns = 12;
+        static int rows = 12;
+        int cellWidth = 30;
+        int cellHeight = 30;
+        string[,] cellGrid = new string[columns, rows];
+        Panel Panel1 = new Panel();
+
+
+        
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            this.Controls.Add(Panel1);
+            Panel1.Location = new Point(0, 0);
+            Panel1.Size = new Size(cellWidth * columns, cellHeight * rows);
+            Panel1.Visible = true;
+
+
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Button cell = new Button();
+                    cellGrid[j, i] = "dead";
+                    cell.Location = new Point((j * cellWidth), (i * cellHeight));
+                    cell.Size = new Size(cellWidth, cellHeight);
+                    cell.BackColor = Color.White;
+                    cell.Click += werdeRot;
+                    Panel1.Controls.Add(cell);
+                }
+
+            }
+        }
+
+        public int Neighbours(int x,int y)
+        {      
+                    int neighbours = 0;
+
+                    for (int k = y - 1; k < y + 2; k++)
+                    {
+                        for (int l = x - 1; l < x + 2; l++)
+                        {
+                            try
+                            {
+                                if (l == y && k == x) 
+                                { 
+                                    neighbours += 0;
+
+                                    
+                                }
+                                else if (cellGrid[l, k] == "Alive") 
+                                { 
+                                    neighbours += 1;
+
+                                   
+                                }
+                            }
+
+                            catch (Exception e)
+                            {
+                                
+                            }
+                            
+                        }
+                    }
+            return neighbours;
+            cellGrid[x, y] = neighbours.ToString();
+        }
+        
+
+        public void werdeRot(object sender, EventArgs e)
+        {
+            Control cell = sender as Control;
+            Button thisButton = ((Button)sender);
+            
+            int xIndex = thisButton.Location.X / cellWidth;
+            int yIndex = thisButton.Location.Y / cellHeight;
+            if (thisButton.BackColor == Color.White)
+            {
+                thisButton.BackColor = Color.Black; 
+                cellGrid[xIndex, yIndex] = "Alive"; 
+
+            }
+
+            else
+            {
+                thisButton.BackColor = Color.White;
+                cellGrid[xIndex, yIndex] = "dead";
+
+            }
+        }
+
+        public async Task nextGrid(object sender, EventArgs e)
+        {
+           
+            foreach (Control cell in Panel1.Controls)
+            {
+                bool isAlive;
+                int xIndex = cell.Location.X / cellWidth;
+                int yIndex = cell.Location.Y / cellHeight;
+                int neighbours = Neighbours(xIndex,yIndex);
+
+                if (neighbours < 2 | neighbours > 3)
+                {
+                    cellGrid[xIndex, yIndex] = "dead";
+                    cell.BackColor = Color.White;
+
+                    btnReset.Text = "Geht nicht";
+                }
+                else if (neighbours == 3)
+                {
+                    cellGrid[xIndex, yIndex] = "Alive";
+                    cell.BackColor = Color.Black;
+
+                    buttonStart.Text = "Get doch";
+                }
+                else if(cellGrid[xIndex, yIndex] == "Alive" && neighbours == 2)
+                {
+                    cellGrid[xIndex, yIndex] = "Alive";
+                    cell.BackColor = Color.Black;
+                }
+
+            }
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            nextGrid(sender ,e);
+            
+        }
+    }
+}
